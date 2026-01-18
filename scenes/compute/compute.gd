d22bfb1c4f714d692c301ed97bb13c9c921bddf9
vilmt <vilmtorm@gmail.com>
2026-01-18 13:01:58 +0200

    Finished compute prototype

--- scenes/compute/compute.gd
+++ scenes/compute/compute.gd
@@ -6,10 +6,8 @@ extends Node3D
 @export var lod_count: int = 5
 
 const HEIGHT_FORMAT := RenderingDevice.DATA_FORMAT_R32_SFLOAT
-
-# TODO: these formats are unsupported
-const NORMAL_FORMAT := RenderingDevice.DATA_FORMAT_R16G16_SNORM
-const CONTROL_FORMAT := RenderingDevice.DATA_FORMAT_R32_UINT
+const NORMAL_FORMAT := RenderingDevice.DATA_FORMAT_R16G16_SFLOAT
+const CONTROL_FORMAT := RenderingDevice.DATA_FORMAT_R32_SFLOAT
 
 var _rd: RenderingDevice
 var _shader_rid: RID
@@ -20,11 +18,18 @@ var _map_rids: Array[RID]
 
 var _uniform_set_rid: RID
 
+var _start_time: int
+@onready var label: Label = $Label
+
 func _ready() -> void:
 	RenderingServer.call_on_render_thread(_initialize)
 
+func _unhandled_key_input(event: InputEvent) -> void:
+	if event.is_action_pressed("compute"):
+		RenderingServer.call_on_render_thread(_compute)
+
 func _exit_tree():
-	RenderingServer.call_on_render_thread(_free_rids)
+	RenderingServer.call_on_render_thread(_free_all_rids)
 
 func _initialize() -> void:
 	_rd = RenderingServer.get_rendering_device()
@@ -43,30 +48,61 @@ func _initialize() -> void:
 	_compute()
 
 func _compute() -> void:
+	_start_time = Time.get_ticks_usec()
+	
 	var compute_list := _rd.compute_list_begin()
 	_rd.compute_list_bind_compute_pipeline(compute_list, _pipeline_rid)
 	_rd.compute_list_bind_uniform_set(compute_list, _uniform_set_rid, 0)
+	
+	var push := PackedFloat32Array()
+	push.append((sin(_start_time) * 0.5 + 0.6) * 50.0)
+	push.append(0.0)
+	push.append(0.0)
+	push.append(0.0)
+	
+	_rd.compute_list_set_push_constant(compute_list, push.to_byte_array(), push.size() * 4)
+	
 	var groups_x := ceili(image_size.x / 8.0)
 	var groups_y := ceili(image_size.y / 8.0)
 	_rd.compute_list_dispatch(compute_list, groups_x, groups_y, lod_count)
 	_rd.compute_list_end()
 	
+	_free_runtime_rids()
+		
 	for texture_rd_rid in _texture_rd_rids:
 		_map_rids.append(RenderingServer.texture_rd_create(texture_rd_rid, RenderingServer.TEXTURE_LAYERED_2D_ARRAY))
+	
+	#print("computed again")
 	
 	if shader_material:
 		var material_rid := shader_material.get_rid()
 		RenderingServer.material_set_param(material_rid, &"_height_maps", _map_rids[0])
 		RenderingServer.material_set_param(material_rid, &"_normal_maps", _map_rids[1])
 		RenderingServer.material_set_param(material_rid, &"_control_maps", _map_rids[2])
+	
+	var elapsed: int = Time.get_ticks_usec() - _start_time
+	label.text = "%s ms" % str(elapsed * 0.001).pad_decimals(1)
 
-func _free_rids() -> void:
+func _free_runtime_rids() -> void:
+	for rid in _map_rids:
+		RenderingServer.free_rid(rid)
+	_map_rids.clear()
+
+func _free_all_rids() -> void:
+	_free_runtime_rids()
+	
 	_rd.free_rid(_pipeline_rid)
 	_rd.free_rid(_uniform_set_rid)
 	_rd.free_rid(_shader_rid)
-	for rid in _map_rids + _texture_rd_rids:
+	for rid in _texture_rd_rids:
 		_rd.free_rid(rid)
 	_rd.free()
+	
+	_pipeline_rid = RID()
+	_uniform_set_rid = RID()
+	_shader_rid = RID()
+	_texture_rd_rids.clear()
+	_rd = null
 
 func _load_shader(path: String) -> RID:
 	var shader_file_data: RDShaderFile = load(path)
--- scenes/compute/compute.glsl
+++ scenes/compute/compute.glsl
@@ -6,8 +6,15 @@
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
 layout(r32f, binding = 0) restrict uniform image2DArray height_maps;
-layout(rg16_snorm, binding = 1) restrict uniform image2DArray normal_maps;
-layout(r32ui, binding = 2) restrict uniform uimage2DArray control_maps;
+layout(rg16f, binding = 1) restrict uniform image2DArray normal_maps;
+layout(r32f, binding = 2) restrict uniform image2DArray control_maps;
+
+layout(push_constant, std430) uniform PushConstants {
+	float amplitude;
+	float pad0;
+	float pad1;
+	float pad2;
+} pc;
 
 void main() {
 	ivec3 texel = ivec3(gl_GlobalInvocationID);
@@ -20,11 +27,12 @@ void main() {
     fnl_state noise = fnlCreateState(1337);
     noise.noise_type = FNL_NOISE_OPENSIMPLEX2;
 	noise.frequency = float(texel.z * 0.01 + 0.01);
-   
-	float h = fnlGetNoise2D(noise, float(texel.x), float(texel.y));
-	//float h = float(texel.x % 2 == 0);
-    imageStore(height_maps, texel, vec4(h, 0.0, 0.0, 0.0));
 	
-	imageStore(normal_maps, texel, vec4(0.5, 1.0, 0.0, 0.0));
-	imageStore(control_maps, texel, uvec4(5u, 0, 0, 0));
+	float h = fnlGetNoise2D(noise, float(texel.x), float(texel.y)) * pc.amplitude;
+	float r = fnlGetNoise2D(noise, float(texel.x + 1), float(texel.y)) * pc.amplitude;
+	float u = fnlGetNoise2D(noise, float(texel.x), float(texel.y + 1)) * pc.amplitude;
+	
+    imageStore(height_maps, texel, vec4(h, 0.0, 0.0, 0.0));
+	imageStore(normal_maps, texel, vec4(h - r, h - u, 0.0, 0.0));
+	imageStore(control_maps, texel, vec4(0.0, 0.0, 0.0, 0.0));
 }
